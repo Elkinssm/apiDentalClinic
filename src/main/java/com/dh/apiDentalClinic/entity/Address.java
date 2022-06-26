b5312c81cd2a12a4881e827406b41cd370215b98
Elkin Silva <52393397+Elkinssm@users.noreply.github.com>
2022-06-26 15:45:30 -0500

    Fix Services and CRUD

--- src/main/java/com/dh/apiDentalClinic/entity/Address.java
+++ src/main/java/com/dh/apiDentalClinic/entity/Address.java
@@ -1,8 +1,10 @@
 package com.dh.apiDentalClinic.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
 
 import javax.persistence.*;
 
@@ -11,6 +13,8 @@ import javax.persistence.*;
 @Getter
 @Entity
 @Table(name="address")
+
+@JsonIgnoreProperties(value={"hibernateLazyInitializer"})
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
